c7a6e6da03adcecf8972776679f2dcb0665c4050
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-15 23:38:55 +0530

    set get all task and create task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    public tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task{
+        let task : Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+
+        this.tasks.push(task);
+
+        return task;
+    }
+}
